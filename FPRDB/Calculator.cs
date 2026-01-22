6772f40c99dfc21cc9dddd138b7985593dc9ca6f
khang <khangau98@example.com>
2026-01-22 21:32:08 +0700

    init FPRDB

---
+++ FPRDB/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication1
+{
+    public class Calculator
+    {
+        // Hàm cộng hai số
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Hàm kiểm tra số chẵn
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        // Hàm chia (để test trường hợp lỗi chia cho 0)
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Không thể chia cho số 0");
+            }
+            return (double)a / b;
+        }
+    }
+}
---
+++ UnitTestProject1/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DXApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            // Arrange (Chuẩn bị)
+            var calc = new Calculator();
+            int a = 5;
+            int b = 3;
+
+            // Act (Hành động)
+            int result = calc.Add(a, b);
+
+            // Assert (Kiểm tra)
+            Assert.AreEqual(8, result); // Kỳ vọng 5 + 3 = 8
+        }
+    }
+}
